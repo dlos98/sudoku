2bd0ce6e5665908beda1d3bf831e860374844f71
dlos98 <43793284+dlos98@users.noreply.github.com>
2023-05-12 13:57:32 -0600

    File for filling-in sudoku function

---
+++ fillSudoku.py
@@ -0,0 +1,15 @@
+#Fill sudoku function
+def fillSudoku(puz):
+    row = input("Enter row number: ")
+    col = input("Enter column number: ")
+    r=int(row)
+    c=int(col)
+    arrayPos = (9*(r-1))+(c-1)
+    if puz[arrayPos] == ' ':
+        val = input("Enter desired value: ")
+        puz[arrayPos]=val
+        printSudoku(puz)
+        return puz
+    else:
+        print("The value in this position is unchangable, please select a new position.")
+        return puz
