f23220a21296cb460e35e62f8d2a95f00fd9c550
dlos98 <43793284+dlos98@users.noreply.github.com>
2023-06-02 17:45:58 -0600

    Soulution check functions deleted from main code

--- sudoku.py
+++ sudoku.py
@@ -3,24 +3,6 @@ from fillSudoku import *
 from solutionCheck import *
 import random
 
-#Check puzzle completion
-def isComplete(sol):
-    for i in range(81):
-        if sol[i] == ' ':
-            return False
-
-#Solution check
-def isSolved(filled, key):
-    isSol = True
-    for i in range(81):
-        if filled[i] != key[i]:
-            isSol = False
-    if isSol == False:
-        print('Sorry, the solution is incorrect. Try again')
-    else:
-        print('Congratulations!')        
-    
-
 ####################################################################################################################################
 
 #PUZZLES
