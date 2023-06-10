1a02d02d6e66cc68a8931b6365d480dff201e156
dlos98 <43793284+dlos98@users.noreply.github.com>
2023-06-09 18:59:23 -0600

    Error handling done for when a non int is input in row and  column

--- fillSudoku.py
+++ fillSudoku.py
@@ -2,14 +2,26 @@ from puzzlePrint import *
 
 #Fill sudoku function
 def fillSudoku(puz):
-    row = int(input("Enter row number (from 1 to 9): "))
+    while True:
+        try:
+            row = int(input("Enter row number (from 1 to 9): "))
+            break
+        except:
+            print('A non-integer was entered')
+    '''row = int(input("Enter row number (from 1 to 9): "))
     while row < 1 or row > 9:
         print(style.BOLD + "Please enter a value between 1 and 9" + style.END)
-        row = int(input("Enter row number (from 1 to 9): "))
-    col = int(input("Enter column number (from 1 to 9): "))
+        row = int(input("Enter row number (from 1 to 9): "))'''
+    while True:
+        try:
+            col = int(input("Enter column number (from 1 to 9): "))
+            break
+        except:
+            print('A non-integer was entered')
+    '''col = int(input("Enter column number (from 1 to 9): "))
     while col < 1 or col > 9:
         print(style.BOLD + "Please enter a value between 1 and 9" + style.END)
-        col = int(input("Enter column number (from 1 to 9): "))
+        col = int(input("Enter column number (from 1 to 9): "))'''
     arrayPos = (9*(row-1))+(col-1)
     if puz[arrayPos] == ' ':
         val = int(input("Enter desired value (from 1 to 9): "))
