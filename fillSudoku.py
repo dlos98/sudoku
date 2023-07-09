077ce40d8753d36ec28d1ce70c46bcdd0463749d
dlos98 <43793284+dlos98@users.noreply.github.com>
2023-07-10 01:58:16 +0900

    Error handling (#7)
    * Error handling done for when a non int is input in row and  column
    
    * Error message fixed and handling added when num not between 1-9 (Position values)
    
    * Handling for position value in all cases and modified in row and col values
    
    * Line 51. row and col variables conv to str to print correctly
    
    * Comment added fillSudoku. Space erased puzzles.csv

--- fillSudoku.py
+++ fillSudoku.py
@@ -2,33 +2,70 @@ from puzzlePrint import *
 
 #Fill sudoku function
 def fillSudoku(puz):
-    row = int(input("Enter row number (from 1 to 9): "))
+    #Row value
+    while True:
+        try:
+            row = int(input("Enter row number (from 1 to 9): "))
+            break
+        except ValueError:
+            print(style.BOLD + 'A non-integer was entered. Try again.' + style.END)
     while row < 1 or row > 9:
         print(style.BOLD + "Please enter a value between 1 and 9" + style.END)
-        row = int(input("Enter row number (from 1 to 9): "))
-    col = int(input("Enter column number (from 1 to 9): "))
+        try:
+            row = int(input("Enter row number (from 1 to 9): "))
+        except ValueError:
+            print(style.BOLD + 'A non-integer was entered. Try again.' + style.END)
+    #Column value
+    while True:
+        try:
+            col = int(input("Enter column number (from 1 to 9): "))
+            break
+        except ValueError:
+            print(style.BOLD + 'A non-integer was entered. Try again.' + style.END)
     while col < 1 or col > 9:
         print(style.BOLD + "Please enter a value between 1 and 9" + style.END)
-        col = int(input("Enter column number (from 1 to 9): "))
+        try:
+            col = int(input("Enter column number (from 1 to 9): "))
+        except ValueError:
+            print(style.BOLD + 'A non-integer was entered. Try again.' + style.END)
+
     arrayPos = (9*(row-1))+(col-1)
+
+    #Position value
     if puz[arrayPos] == ' ':
-        val = int(input("Enter desired value (from 1 to 9): "))
+        while True:
+            try:
+                val = int(input("Enter desired value (from 1 to 9): "))
+                break
+            except ValueError:
+                print(style.BOLD + 'A non-integer was entered. Try again.' + style.END)
         while val < 1 or val > 9:
             print(style.BOLD + "Please enter a value between 1 and 9." + style.END)
-            val = int(input("Enter desired value (from 1 to 9): "))
+            try:
+                val = int(input("Enter desired value (from 1 to 9): "))
+            except ValueError:
+                print(style.BOLD + 'A non-integer was entered. Try again.' + style.END)
         puz[arrayPos]=val
         printSudoku(puz)
         return puz
     elif type(puz[arrayPos]) == int:
-        print(style.BOLD + "You are changing the previous value of row " + row + " and column " + col + style.END)
-        val = int(input("Enter desired value (from 1 to 9): [If you do not wish to change ths value enter '0']"))
+        print(style.BOLD + "You are changing the previous value of row " + str(row) + " and column " + str(col) + style.END)
+        while True:
+            try:
+                val = int(input("Enter desired value (from 1 to 9): [If you do not wish to change ths value enter '0']"))
+                break
+            except ValueError:
+                print(style.BOLD + 'A non-integer was entered. Try again.' + style.END)
         if val == 0:
             printSudoku(puz)
             return puz
         else:
             while val < 1 or val > 9:
                 print(style.BOLD + "Please enter a value between 1 and 9." + style.END)
-                val = int(input("Enter desired value (from 1 to 9): "))
+                try:
+                    val = int(input("Enter desired value (from 1 to 9): "))
+                except:
+                    print(style.BOLD + 'A non-integer was entered. Try again.' + style.END)
             puz[arrayPos]=val
             printSudoku(puz)
             return puz
